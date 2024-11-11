13396d5ce9128f5ccae53be4043d38ad39483408
misagul <isagul7890@gmail.com>
2024-11-11 15:44:47 +0300

    Create all.sh

---
+++ all.sh
@@ -0,0 +1,6 @@
+helm repo add jenkinsci https://charts.jenkins.io
+helm repo update
+kubectl apply -f namespace.yaml
+kubectl apply -f jenkins-volume.yaml
+kubectl apply -f jenkins-sa.yaml
+helm install jenkins -n jenkins -f jenkins-values.yaml jenkinsci/jenkins
