5c50597e7760076b5de3e0b77671636429459857
misagul <isagul7890@gmail.com>
2024-11-11 16:11:08 +0300

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,3 +1,6 @@
+mkdir /data
+mkdir /data/jenkins-volume
+sudo chown -R 1000:1000 /data/jenkins-volume
 helm repo add jenkinsci https://charts.jenkins.io
 helm repo update
 kubectl apply -f namespace.yaml
