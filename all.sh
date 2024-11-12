866c6fecef389d020a681a2933a21f4dbd29752d
M. Isa Gul <71321835+misagul@users.noreply.github.com>
2024-11-12 08:50:55 +0300

    Update all.sh

--- all.sh
+++ all.sh
@@ -9,3 +9,4 @@ kubectl apply -f jenkins-sa.yaml
 helm install jenkins -n jenkins -f jenkins-values.yaml jenkinsci/jenkins
 echo "Waiting for the pod to be ready..."
 kubectl wait --for=condition=Ready --timeout=-1s $(kubectl get pods -n jenkins -o name | grep '^pod/jenkins') -n jenkins
+kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/additional/chart-admin-password && echo
