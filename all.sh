ccc582ab9fbead188f7affd5d838446a5d97c14f
M. Isa Gul <71321835+misagul@users.noreply.github.com>
2024-11-13 09:45:26 +0300

    Update all.sh

--- all.sh
+++ all.sh
@@ -4,4 +4,5 @@ kubectl apply -f namespace.yaml
 helm install jenkins jenkins/jenkins -n jenkins -f values.yaml 
 echo "Waiting for the pod to be ready..."
 kubectl wait --for=condition=Ready --timeout=-1s $(kubectl get pods -n jenkins -o name | grep '^pod/jenkins') -n jenkins
-kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/additional/chart-admin-password && echo
+echo port: $(kubectl get svc jenkins -n jenkins -o jsonpath="{.spec.ports[0].nodePort}")
+echo password: $(kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/additional/chart-admin-password)
